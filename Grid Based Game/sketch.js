f693951c16d14f8a788652ef8a68822e50d3e8d1
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2021-10-21 17:27:09 -0600

    generating random blocks

--- Grid Based Game/sketch.js
+++ Grid Based Game/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let number = 0;
 let grid  = [];
 let gridSize = 10;
 let gridSide;
@@ -34,12 +35,35 @@ function create2DArray() {
 function displayGrid() {
   for (let x = 0; x < gridSize; x++) {
     for (let y = 0; y < gridSize; y++) {
+      if (grid[x][y] === 0) {
+        fill("white");
+      }
+
+      else if (grid[x][y] === 1) {
+        fill("black");
+      }
+
       rect(x * gridSide, y * gridSide, gridSide, gridSide);
     }
   }
 }
 
-function generateBlock() {}
+function generateBlock() {
+  //number += 1;
+  let rowOneLength = random(0,3);
+  let rowTwoLength = random(0,3);
 
+  let rowOnePosition = round(random(1, gridSize-rowOneLength));
+  let rowTwoPosition = round(random(rowOnePosition, rowOnePosition + rowOneLength));
+
+  for (let x = 0; x < rowOneLength; x++) {
+    grid[x + rowOnePosition-1][0] = 1;
+  }
+
+  for (let x = 0; x < rowTwoLength; x++) {
+    grid[x + rowTwoPosition-1][1] = 1;
+  }
+}
 function mousePressed() {
-  
+  generateBlock();
+}
