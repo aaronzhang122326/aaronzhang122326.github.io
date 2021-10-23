f58bf12b750369fc4223e85f0f635a5df2baff9b
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2021-10-22 21:27:09 -0600

    blocks can move down automatically

--- Grid Based Game/sketch.js
+++ Grid Based Game/sketch.js
@@ -11,21 +11,26 @@ let newGrid;
 let gridHeight = 20;
 let gridWidth = 10;
 let gridSide;
+let blockList;
+let blockListZ = -1;
+
 
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
-
+  
   grid = create2DArray();
   gridSide = height/gridHeight;
-  
-  //create2DArray();
+  newGrid = create2DArray();
+  blocklist = [];
 }
+
+
 
 function draw() {
   background(220);
   displayGrid();
-  // rect(mouseX, mouseY, 100, 50);
+  moveDown();
 }
 function create2DArray() {
   let screen = [];
@@ -55,36 +60,61 @@ function displayGrid() {
 }
 
 function generateBlock() {
-  //number += 1;
+  
   let rowOneLength = random(0,3);
   let rowTwoLength = random(0,3);
 
-  let rowOnePosition = round(random(1, gridWidth-rowOneLength));
-  let rowTwoPosition = round(random(rowOnePosition, rowOnePosition + rowOneLength -1));
+
+
+  let blockListX = 0;
+
+  blockListZ += 1;
+  blockList.push([]);
+  blockList[blockListZ].push([],[]);
 
   for (let x = 0; x < rowOneLength; x++) {
-    grid[0][x + rowOnePosition-1] = 1;
+    blockList[blockListZ][blockListY].push(1);
+    blockListY += 1;
+    // grid[0][x + rowOnePosition-1] = 1;
   }
 
   for (let x = 0; x < rowTwoLength; x++) {
-    grid[1][x + rowTwoPosition-1] = 1;
+    blockList[blockListZ][blockListY].push(1);
+    // grid[1][x + rowTwoPosition-1] = 1;
   }
+}
+
+function displayBlocks() { 
+  let rowOnePosition = round(random(1, gridWidth-rowOneLength));
+  let rowTwoPosition = round(random(rowOnePosition, rowOnePosition + rowOneLength -1));
+  
+  for (let y = 0; y < 2; y++) {
+    for (let x = 0; x < blockList[blockListZ][y]; x++) {
+      
+        grid[y+rowOnePosition][x] = 
+    }
+}
 }
 
 function moveDown() {
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === 1) {
-        newGrid[y+1][x] = 1;
-        newGrid[y][x] = 0;
+  if (frameCount % 20 === 0) { 
+    for (let y = grid.length-1; y >= 0; y--) {
+      for (let x = 0; x < grid[y].length; x++) {
+        if (grid[y][x] === 1) {
+          newGrid[y+1][x] = 1;
+          newGrid[y][x] = 0;
+        }
       }
     }
+    grid = [...newGrid];
   }
-  grid = newGrid;
 }
 
 function mousePressed() {
-  newGrid = grid;
   generateBlock();
-  moveDown();
+  
+}
+
+function keyPressed() {
+  // moveDown();
 }
