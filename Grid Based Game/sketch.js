78f5f42050eb88bb9e4d35292b6c719331830de7
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2021-10-23 18:54:32 -0600

    new method of moving blocks down
    change the method of moving blocks down for future purposes

--- Grid Based Game/sketch.js
+++ Grid Based Game/sketch.js
@@ -13,7 +13,46 @@ let gridWidth = 10;
 let gridSide;
 let blockList;
 let blockListZ = -1;
+let blockListY = 0;
+let positionY = 0;
+let positionX;
+let start = false;
 
+let iBlocks = [
+  [1,1,1,1,1],
+];
+
+let jBlocks = [
+  [1,0,0,0],
+  [1,1,1,1],
+];
+              
+let lBlocks = [
+  [0,0,0,1],
+  [1,1,1,1],
+];
+
+let oBlocks = [
+  [1,1],
+  [1,1],
+];
+
+let tBlocks = [
+  [0,1,0],
+  [1,1,1],
+];
+
+let sBlocks = [
+  [0,1,1],
+  [1,1,0],
+];
+
+let zBlocks = [
+  [1,1,0],
+  [0,1,1],
+];
+
+let blockChoices = [iBlocks, jBlocks, lBlocks, oBlocks, tBlocks, sBlocks, zBlocks];
 
 function setup() {
 
@@ -22,7 +61,7 @@ function setup() {
   grid = create2DArray();
   gridSide = height/gridHeight;
   newGrid = create2DArray();
-  blocklist = [];
+  blockList = [];
 }
 
 
@@ -31,6 +70,7 @@ function draw() {
   background(220);
   displayGrid();
   moveDown();
+  console.log(frameCount % 20 === 0);
 }
 function create2DArray() {
   let screen = [];
@@ -59,54 +99,67 @@ function displayGrid() {
   }
 }
 
+// function generateBlock() {
+  
+//   let rowOneLength = random(0,3);
+//   let rowTwoLength = random(0,3);
+
+
+//   // blockList = [];
+//   let blockListX = 0;
+
+//   blockListZ += 1;
+//   blockList.push([]);
+//   blockList[blockListZ].push([],[]);
+
+//   for (let x = 0; x < rowOneLength; x++) {
+//     blockList[blockListZ][blockListY].push(1);
+//     blockListY += 1;
+//     // grid[0][x + rowOnePosition-1] = 1;
+//   }
+
+//   for (let x = 0; x < rowTwoLength; x++) {
+//     blockList[blockListZ][blockListY].push(1);
+//     // grid[1][x + rowTwoPosition-1] = 1;
+//   }
+// }
+
 function generateBlock() {
-  
-  let rowOneLength = random(0,3);
-  let rowTwoLength = random(0,3);
-
-
-
-  let blockListX = 0;
-
+  blockList.push(random(blockChoices));
   blockListZ += 1;
-  blockList.push([]);
-  blockList[blockListZ].push([],[]);
 
-  for (let x = 0; x < rowOneLength; x++) {
-    blockList[blockListZ][blockListY].push(1);
-    blockListY += 1;
-    // grid[0][x + rowOnePosition-1] = 1;
-  }
-
-  for (let x = 0; x < rowTwoLength; x++) {
-    blockList[blockListZ][blockListY].push(1);
-    // grid[1][x + rowTwoPosition-1] = 1;
-  }
+  positionY = 0;
+  positionX = round(random(0, gridWidth-blockList[blockListZ][0].length));
+  start = true;
 }
 
-function displayBlocks() { 
-  let rowOnePosition = round(random(1, gridWidth-rowOneLength));
-  let rowTwoPosition = round(random(rowOnePosition, rowOnePosition + rowOneLength -1));
+// function displayBlocks() { 
+//   let rowOnePosition = round(random(1, gridWidth-rowOneLength));
+//   let rowTwoPosition = round(random(rowOnePosition, rowOnePosition + rowOneLength -1));
   
-  for (let y = 0; y < 2; y++) {
-    for (let x = 0; x < blockList[blockListZ][y]; x++) {
-      
-        grid[y+rowOnePosition][x] = 
-    }
-}
-}
+  
+//     }
+//   }
+// }&& blockList[blockListZ][], blockList[blockListZ][0].length
 
 function moveDown() {
-  if (frameCount % 20 === 0) { 
-    for (let y = grid.length-1; y >= 0; y--) {
-      for (let x = 0; x < grid[y].length; x++) {
-        if (grid[y][x] === 1) {
-          newGrid[y+1][x] = 1;
-          newGrid[y][x] = 0;
+  if (start === true) {
+    if (frameCount % 20 === 0 && positionY + blockList[blockListZ].length <= gridHeight) {   
+      for (let y = blockList[blockListZ].length -1; y >= 0; y--) {
+        for (let x = 0; x < blockList[blockListZ][y].length; x++) {
+          grid[y+positionY][x+positionX] = blockList[blockListZ][y][x]; 
+          if (positionY > 0){
+            grid[y+positionY-1][x+positionX] = 0;
+          }
         }
-      }
+      } 
+      grid = [...newGrid];
+      positionY += 1;
     }
-    grid = [...newGrid];
+  }
+
+  else {
+    start = false;
   }
 }
 
