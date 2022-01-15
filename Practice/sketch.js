e0303222501f28473198ce388d30de8af0f9b575
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2022-01-14 18:09:14 -0600

    experiment

--- Practice/sketch.js
+++ Practice/sketch.js
@@ -6,7 +6,14 @@ function draw() {
   
 }
 function mousePressed(){
-  console.log(round(random(-20, 20)) + "   " + round(random(-20, 20)));
+  console.log(round(random(-20, 0)) + " - " + "(-" + round(random(0, 20)) + ") =");
+  // if ((round(random(1,2)) === 1)){
+  //   console.log(round(random(-20, 0)) + " - " + "(-" + round(random(0, 20)) + ") =");
+  // }
+  // else {
+  //   console.log(round(random(-20, 0)) + " + " + "(-" + round(random(0, 20)) + ") =");
+  // }
+
   //console.log(round(random(0, 20)) + "   " + round(random(0, 20)));
 }
   
