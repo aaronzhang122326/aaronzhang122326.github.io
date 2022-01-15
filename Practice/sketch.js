fe0cebf7315d7fa50997dbb50071cc692c1c7994
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2022-01-15 17:36:37 -0600

    experiment

--- Practice/sketch.js
+++ Practice/sketch.js
@@ -6,13 +6,44 @@ function draw() {
   
 }
 function mousePressed(){
-  console.log(round(random(-20, 0)) + " - " + "(-" + round(random(0, 20)) + ") =");
+  //console.log(round(random(0, 20)) + " - " + "(-" + round(random(0, 20)) + ") =");
   // if ((round(random(1,2)) === 1)){
-  //   console.log(round(random(-20, 0)) + " - " + "(-" + round(random(0, 20)) + ") =");
+  //   console.log("-"  + round(random(0, 10)) + " + " + round(random(10, 20)) + " =");
   // }
   // else {
-  //   console.log(round(random(-20, 0)) + " + " + "(-" + round(random(0, 20)) + ") =");
+  //   console.log(round(random(0, 10)) + " - " + round(random(10, 20)) + " =");
   // }
+
+  if ((round(random(1,8)) === 1)){
+    console.log("-"  + round(random(10, 20)) + " + " + round(random(0, 10)) + " =");
+  }
+  else if ((round(random(1,8)) === 2)){
+    console.log(round(random(10, 20)) + " + " + "(-" + round(random(0, 10)) + ") =");
+  }
+  else if ((round(random(1,8)) === 3)){
+    console.log("-" + round(random(10, 20)) + " + " + "(-" + round(random(0, 10)) + ") =");
+  }
+  else if ((round(random(1,8)) === 4)){
+    console.log("-" + round(random(10, 20)) + " - " + "(-" + round(random(0, 10)) + ") =");
+  }
+  else if ((round(random(1,8)) === 5)){
+    console.log("-" + round(random(10, 20)) + " - " + round(random(0, 10)) + " =");
+  }
+  else if ((round(random(1,8)) === 6)){
+    console.log(round(random(10, 20)) + " - " + "(-" + round(random(0, 10)) + ") =");
+  }
+  else if ((round(random(1,8)) === 5)){
+    console.log("-" + round(random(10, 20)) + " - " + round(random(0, 10)) + " =");
+  }
+  else if ((round(random(1,8)) === 6)){
+    console.log(round(random(10, 20)) + " - " + "(-" + round(random(0, 10)) + ") =");
+  }
+  else if ((round(random(1,8)) === 7)){
+    console.log("-" + round(random(0, 10)) + " + " + round(random(10, 20)) + " =");
+  }
+  else if ((round(random(1,8)) === 8)){
+    console.log(round(random(0, 10)) + " - " + round(random(0, 20)) +  "=");
+  }
 
   //console.log(round(random(0, 20)) + "   " + round(random(0, 20)));
 }
